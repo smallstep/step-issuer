32f66b86f693c4768a1c20598c4cb1a562db6c35
Christian Wolf <bc.christianwolf@googlemail.com>
2021-04-27 19:29:37 +0200

    create files for stepclusterissuer

--- main.go
+++ main.go
@@ -99,6 +99,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.StepClusterIssuerReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("StepClusterIssuer"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "StepClusterIssuer")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
